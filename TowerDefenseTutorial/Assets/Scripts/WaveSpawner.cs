18f4f074a445431470ee8a737b8013cb40bacad9
fanyixin0428 <fanyixin0428@gmail.com>
2021-04-03 20:47:11 +0800

    出怪

---
+++ TowerDefenseTutorial/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+    
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpwanEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        //numOfEnemies = Waves[waveNumber].count;
+
+        //numOfEnemies = waveNumber * waveNumber + 1;
+
+        ;
+
+    }
+
+    void SpwanEnemy()
+    {
+        Instantiate(enemyPrefab,spawnPoint.position,spawnPoint.rotation);
+
+    }
+
+
+  
+
+}
