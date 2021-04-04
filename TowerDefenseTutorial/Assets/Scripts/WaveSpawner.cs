f356e5323be666e8e5b78fb7f5a050885a730efd
fanyixin0428 <fanyixin0428@gmail.com>
2021-04-04 12:25:06 +0800

    导入炮台

--- TowerDefenseTutorial/Assets/Scripts/WaveSpawner.cs
+++ TowerDefenseTutorial/Assets/Scripts/WaveSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
@@ -8,6 +9,8 @@ public class WaveSpawner : MonoBehaviour
     
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
+
+    public Text waveCountdownText;
 
     private int waveIndex = 0;
 
@@ -20,6 +23,8 @@ public class WaveSpawner : MonoBehaviour
         }
 
         countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
     }
 
     IEnumerator SpawnWave()
