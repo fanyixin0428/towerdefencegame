8860942ec381ab1e0d7143eb864653b1764f0616
fanyixin0428 <fanyixin0428@gmail.com>
2021-04-05 11:38:53 +0800

    建造第一个防御塔

---
+++ TowerDefenseTutorial/Assets/Scripts/Node.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+
+    private GameObject turret;
+
+   
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        
+        if (turret != null)
+        {
+
+            Debug.Log("Can't build there! - ToDo :Display on screen.");
+            return;
+        }
+        //Build a turret
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
+
+    }
+
+
+
+    void OnMouseEnter()
+    {
+
+        rend.material.color = hoverColor;
+
+
+    }
+
+    void OnMouseExit()
+    {
+            rend.material.color = startColor;
+    }
+
+
+
+}
